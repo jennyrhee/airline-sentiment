aadaf7155ed583ed5096704c0839293bf8d2a21f
jennyrhee <jennyirhee@gmail.com>
2020-06-23 09:37:44 -0500

    redirect to prediction.html

--- website/app.py
+++ website/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, Markup
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -11,6 +11,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        twitter_handle = request.form['text_in']
+        return redirect(url_for('prediction',
+                                twitter_handle='@'+twitter_handle))
+
     return render_template('index.html')
 
 
