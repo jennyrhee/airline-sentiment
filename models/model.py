8ace3109428898fb80cc7b3b95fcd69092b24ba7
jennyrhee <jennyirhee@gmail.com>
2020-07-02 09:12:22 -0500

    restore model.py to git from lfs

--- models/model.py
+++ models/model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28da46b388de407861ea1336421798feaca98dcc9656133a60443c9691afffd6
-size 5663
+oid sha256:d4beecc79a48483fcf12cca85827762de9ce9b58062c8603a98da7c1f2fe96eb
+size 6697
